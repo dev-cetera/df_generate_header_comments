e248760fcb6b35bb1b7217060bdf71beba416f87
robmllze <86869208+robmllze@users.noreply.github.com>
2024-10-04 22:09:59 +1000

    +breaking: Update dependencies and docs

--- bin/genhdr-v1.dart
+++ bin/genhdr-v1.dart
@@ -1,5 +1,14 @@
-// The use of this source code is governed by the LICENSE file located in this
-// project's root directory.
+//.title
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//
+// Dart/Flutter (DF) Packages by DevCetra.com & contributors. The use of this
+// source code is governed by an MIT-style license described in the LICENSE
+// file located in this project's root directory.
+//
+// See: https://opensource.org/license/mit
+//
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//.title~
 
 import 'package:df_generate_header_comments/df_generate_header_comments.dart';
 import 'package:df_log/df_log.dart';
